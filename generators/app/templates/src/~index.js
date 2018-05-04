4f31d0da02cba08e5eb9f315e86e80fc3a7997ca
lvpengyue1 <245745113@qq.com>
2018-05-04 09:49:55 +0800

    修改了移动端组件默认为vant

--- generators/app/templates/src/~index.js
+++ generators/app/templates/src/~index.js
@@ -1,8 +1,9 @@
 import Vue from 'vue';<% if (platform === 'PC') { %>
 import iView from 'iview';
 import 'iview/dist/styles/iview.css';<% } else if (platform === 'mobile') { %>
-import MintUI from 'mint-ui';
-import 'mint-ui/lib/style.css';<% } %>
+import Vant from 'Vant';
+import 'Vant/lib/vant-css/index.css';<% } %>
+import 'Vant/lib/vant-css/icon-local.css';<% } %>
 import $router from './components/utils/$router';
 import $store from './components/utils/$store';
 import $app from './components/views/$app/index.vue';
@@ -11,7 +12,7 @@ import $app from './components/views/$app/index.vue';
 
 // 加载组件库<% if (platform === 'PC') { %>
 Vue.use(iView);<% } else if (platform === 'mobile') { %>
-Vue.use(MintUI);<% } %>
+Vue.use(Vant);<% } %>
 
 // 启动应用
 // 创建系统根组件（vm）
